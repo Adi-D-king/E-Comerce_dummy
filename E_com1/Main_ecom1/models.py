93f00834eac721b75ec89fc00e2d619b54edd271
Adi-D-king <as4497119@gmail.com>
2024-09-23 14:19:09 +0530

    Miodels created

--- E_com1/Main_ecom1/models.py
+++ E_com1/Main_ecom1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Accounts(models.Model):
+    accounts_id = models.IntegerField()
+    user_name = models.CharField(max_length=20)
+    e_mail = models.CharField(max_length=20)
+    phone_no = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.accounts_id
+
