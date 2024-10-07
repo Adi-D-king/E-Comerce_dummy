bebe45177aba2a9e1c3fb87c33a7e174b1308cfd
Adi-D-king <as4497119@gmail.com>
2024-10-07 11:07:17 +0530

    login authentication updated

--- ProjectRelic/users/views.py
+++ ProjectRelic/users/views.py
@@ -2,47 +2,49 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,HttpResponseRedirect
 from .forms import Userregestration
 from .models import Accounts
+from shop.models import Products
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
+from django.contrib.auth.hashers import make_password,check_password
 
 # Create your views here.
 
 def login(request):
-    if request.method == 'Post':
-        e_mail = request.POST.get('E_mail')
-        password = request.POST.get('passowrd')
-        confirm_password = request.POST.get('confirm_password')
+    products = Products.objects.all()
+    if request.method == 'POST':
+        e_mail = request.POST.get('e_mail')
+        password = request.POST.get('password')
         account = get_object_or_404(Accounts,e_mail=e_mail)
-        if password == confirm_password:
-            if password == account.password:
-                return render(request,'shop/index.html',{'account':account})
-            else:
-                return redirect('404')
+        if account.password == password:
+            # return HttpResponseRedirect('index',{'products':products,'account':account})
+            # return redirect('index')
+            return render(request,'shop/index.html',{'products':products,'account':account})
         else:
-            return redirect("worng passowrd")
-    else:
-        return render(request,'users/login.html')
+            return HttpResponse("ERROR")
+    
+    return render(request,'users/login.html')
         
         
 def singup(request):
     if request.method == 'POST':
-        print(request)
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
         e_mail = request.POST.get('e_mail')
         phone_nub = request.POST.get('phone_number')
 
-        print(first_name,last_name,e_mail,phone_nub)
-
         form_data = Accounts(firstname=first_name,lastname=last_name,e_mail=e_mail,phone_no=phone_nub)
-        form_data.save()
-        return redirect('login')
+        # form_data.save()
+        # try:
+        #     form_data.save()
+        # except:
+        #     return 'Account alredy exiests !plese login.'
+        # return redirect('login')
     else:
         print(request)
         return render(request,'users/singup.html')
 
 def verify_email(request):
-    email = request.GET.get('email')
+    email = request.POST.get('email')
     response_data = {
             'is_taken': Accounts.objects.filter(e_mail=email).exists()
     }
